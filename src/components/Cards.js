dc84c0a177ee3ad6e41662df352cc5db8db6f9b8
Abolfazl <abolfazlghanbarishce@gmail.com>
2021-02-16 16:05:35 +0330

    add footer

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
 function Cards() {
     return (
-        <div className="cards">
-            <h1>Check out these EPIC destinations!</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__item">
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
                         <CardItem
                             src='images/img-9.jpg'
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
@@ -45,7 +45,7 @@ function Cards() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Cards
+export default Cards;
